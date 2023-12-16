e1d33582c6d5f6e2d835d43e3f3489644cb075cc
sampathNG <jnanisampath@gmail.com>
2023-12-16 21:53:36 +0530

    _

--- day.py
+++ day.py
@@ -1,5 +1,6 @@
-print("hello world !")
-print("hello")
-print("world")
-print("xcv")
-print("qazqwerty")
+# Find the sum of all the multiples of 3 or 5 below 100
+# sum = 0
+# for i in range(1,101):
+#     if i % 3 == 0 or i % 5 == 0:
+#         sum = sum+i
+# print(sum)
